11c12855af442e0d4dd42e2eb2a1732e3a7ee520
chenjian <chen.jian@3g2win.com>
2016-11-04 11:22:59 +0800

    新增加setHost 和 getHost 接口来设置地址和 获取地址/新增加setHost 和 getHost 接口来设置地址和 获取地址

--- EUExLog/EUExLog/EUExLog.m
+++ EUExLog/EUExLog/EUExLog.m
@@ -9,11 +9,18 @@
 #import "EUExLog.h"
 #import "EUtility.h"
 #import "BUtility.h"
+#import "JSON.h"
+
+#import "WWidget.h"
+#import "EBrowserView.h"
+#import "UexLogMySingleton.h"
+
 #define LOG_PORT 30050
 
 @implementation EUExLog
 
 GCDAsyncUdpSocket *sockObj;
+
 -(id)initWithBrwView:(EBrowserView *) eInBrwView{	
 	if (self = [super initWithBrwView:eInBrwView]) {	
 	}
@@ -21,32 +28,102 @@ GCDAsyncUdpSocket *sockObj;
 }
 
 -(void)dealloc{
+    
 	if(sockObj){
+        
 		[sockObj release];
+        
 		sockObj = nil;
 	}
+    
 	[super dealloc];
+    
 }
 
 - (void)clean {
+    
     if (meBrwView) {
+        
         meBrwView = nil;
     }
 }
 
 -(void)sendLog:(NSMutableArray *)inArguments{
+    
+    UexLogMySingleton * UexLog = [UexLogMySingleton shareMySingLeton];
+    
 	NSString *inLog = [inArguments objectAtIndex:0];
+    
 	NSString *logServerIp = [EUtility LogServerIp:meBrwView];
+    
     if (!logServerIp) {
+        
         logServerIp = [BUtility getMainWidgetConfigLogserverip];
     }
-	if (logServerIp) {
+    
+    if (UexLog.hostAddress) {
+        
+        NSLog(@"setHost地址：+++%@",UexLog.hostAddress);
+        [sockObj sendData:[inLog dataUsingEncoding:NSUTF8StringEncoding] toHost:self.hostAddress port:LOG_PORT withTimeout:-1 tag:0];
+        
+    } else if (logServerIp) {
+        
 		if (sockObj==nil) {
+            
 			sockObj = [[GCDAsyncUdpSocket alloc] init];
 		}
+        
         if (sockObj) {
+            
             [sockObj sendData:[inLog dataUsingEncoding:NSUTF8StringEncoding] toHost:logServerIp port:LOG_PORT withTimeout:-1 tag:0];
+            NSLog(@"++++++++%@",logServerIp);
         }
-	}
+    
+    }
 }
+
+-(void)setHost:(NSMutableArray*)array{
+    
+    UexLogMySingleton * UexLog = [UexLogMySingleton shareMySingLeton];
+
+    NSString * hostString = [array objectAtIndex:0];
+    
+    NSDictionary * hostDict = [hostString JSONValue];
+    
+    WWidget *inCurWgt = self.meBrwView.mwWgt;
+    
+    if ([[hostDict objectForKey:@"appdebug"] isEqualToString:@"1"]) {
+        
+        inCurWgt.isDebug = YES;
+        
+    } else {
+        
+        inCurWgt.isDebug = NO;
+    }
+    
+    UexLog.hostAddress = [hostDict objectForKey:@"logserverip"];
+    
+    NSUserDefaults * logUserDefaults = [NSUserDefaults standardUserDefaults];
+    
+    [logUserDefaults setObject: UexLog.hostAddress forKey:@"logserverip"];
+
+}
+
+-(void)getHost:(NSMutableArray*)array{
+    
+    NSUserDefaults * logUserDefaults = [NSUserDefaults standardUserDefaults];
+    
+    NSString * getHostStr = [logUserDefaults objectForKey:@"logserverip"];
+    
+    NSString *jsStr = [NSString stringWithFormat:@"if(uexLog.cbGetHost!=null){uexLog.cbGetHost('%@');}", getHostStr];
+    
+    [self.meBrwView stringByEvaluatingJavaScriptFromString:jsStr];
+    
+}
+
+
+
+
+
+
 @end
